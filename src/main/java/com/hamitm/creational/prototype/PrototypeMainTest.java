7f598843334d7bc2abcb683002af66b68f947e2d
HamitMizrak <hamitmizrak@gmail.com>
2021-07-07 10:21:10 +0300

    prototype revize

--- src/main/java/com/hamitm/creational/prototype/PrototypeMainTest.java
+++ src/main/java/com/hamitm/creational/prototype/PrototypeMainTest.java
@@ -3,9 +3,6 @@ package com.hamitm.creational.prototype;
 public class PrototypeMainTest {
 	
 	public static void main(String[] args) {
-		//MultipleDatabasePrototype databases=new MultipleDatabasePrototype();
-		//System.out.println(databases.getUrl());
-		//System.out.println(databases.getDbName());
 		
 		MultipleDatabasePrototype mysql=new Mysql();
 		System.out.println(mysql.hashCode());
@@ -18,7 +15,6 @@ public class PrototypeMainTest {
 			mssql.setDbName("mssql");
 			System.out.println(mssql);
 		} catch (CloneNotSupportedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
