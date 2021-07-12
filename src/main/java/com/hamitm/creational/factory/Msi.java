3d005ef0868ca493eff0e4430d27170805132993
HamitMizrak <hamitmizrak@gmail.com>
2021-07-12 14:32:31 +0300

    4-)Factory Design Pattern(creation)

---
+++ src/main/java/com/hamitm/creational/factory/Msi.java
@@ -0,0 +1,17 @@
+package com.hamitm.creational.factory;
+
+public class Msi implements IBilgisayar {
+
+	@Override
+	public String getComputerName() {
+		return "MSi";
+	}
+
+	@Override
+	public String toString() {
+		return "Msi [getComputerName()=" + getComputerName() + "]";
+	}
+	
+	
+	
+}
