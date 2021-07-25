b1fed71266541829506e6cd1e4660a10c18503d1
HamitMizrak <hamitmizrak@gmail.com>
2021-07-25 11:11:58 +0300

    2-) Behavioral Template Design Pattern

---
+++ src/main/java/com/hamitm/behavioral/template/TemplateMainTest.java
@@ -0,0 +1,17 @@
+package com.hamitm.behavioral.template;
+
+public class TemplateMainTest {
+	
+	public static void main(String[] args) {
+		
+		Okul okul=new OzelSektor();
+		okul.mezun();
+		
+		System.out.println("**************************************");
+		
+		okul=new Devlet();
+		okul.mezun();
+		
+	}
+	
+}
